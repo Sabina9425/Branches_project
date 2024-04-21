2a868ed0731aec45da45b0b2fc6af14798b178d4
Sabina9425 <visapa@icloud.com>
2024-04-21 20:40:02 +0400

    added docstrings for function convert to upper case

--- src/main/str_func.py
+++ src/main/str_func.py
@@ -1,4 +1,8 @@
 def convert_to_upper_case(txt):
+    """"This function converts letters to upper case
+    :param txt: input words
+    :return: converted words
+    """
     return txt.upper()
 
 def convert_every_first_letter_to_upper_case(txt):
