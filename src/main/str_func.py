aec98bc48694fcd0b585e6fde5e75b137e11f7b6
Sabina9425 <visapa@icloud.com>
2024-04-21 20:32:45 +0400

    added function to convert every first letter to upper case

--- src/main/str_func.py
+++ src/main/str_func.py
@@ -1,2 +1,10 @@
 def convert_to_upper_case(txt):
     return txt.upper()
+
+def convert_every_first_letter_to_upper_case(txt):
+    """"This fucntion converts every first letter of every word to upper case
+    :param txt: input sentence
+    :return: converted sentence
+    """
+    return txt.capwords()
+
